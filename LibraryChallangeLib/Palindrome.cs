5d6acc1258acacb33a6f6b239c0e13843df52b2b
John Filak <jf3163@nyu.edu>
2020-01-13 10:47:18 -0600

    Coding Challange SLN

---
+++ LibraryChallangeLib/Palindrome.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+namespace LibraryChallangeLib
+{
+    public class Palindrome 
+    {
+        public bool isPalidrome(string a)
+        {
+
+            //Regex rx = new Regex("[^A-Za-z0-9 _]");
+            string b = a.Replace(" ",  "");
+            string c = b.Replace(".", "");
+            string d = c.Replace(",", "");
+            string e = d.ToLower();
+            Char[] final = e.ToCharArray();
+
+            for (int i = 0; i<final.Length; i++)
+            { int end = (final.Length - (i + 1));
+                char a1 = final[i];
+                char a2 = final[end];
+
+                if ( (a1.Equals(a2)) == false)
+                {
+                    break;
+                }
+                if(i == end)
+                {
+                    return true; 
+                }
+            }
+
+
+            return false;
+        }
+        
+    }
+}
---
+++ TestingChallangexUbit/PalindromeTest.cs
@@ -0,0 +1,21 @@
+using System;
+using LibraryChallangeLib;
+using Xunit;
+
+
+namespace TestingChallangexUnit
+{
+    public class PalindromeTest
+    {
+         readonly Palindrome P  = new Palindrome();
+
+        [Fact]
+        public void PalindromeIsPalindrome()
+        {
+            string test = ("nurses run");
+            bool value = P.isPalidrome(test);
+            Assert.Equal(true, value);
+        }
+
+    }
+}
